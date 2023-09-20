f7dc846552d52198c08445edf6a3bcce471e4c63
Henrique Trajano <henrique.araujo@contmatic.com.br>
2023-09-20 01:32:33 -0300

    created services for Car and Owner

---
+++ Main-Spring-Api/src/main/java/br/com/trajy/mainspring/api/service/CarPostStoreService.java
@@ -0,0 +1,27 @@
+package br.com.trajy.mainspring.api.service;
+
+import br.com.trajy.mainspring.api.client.CarPostStoreClient;
+import br.com.trajy.mainspring.api.model.dto.CarPostDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CarPostStoreService {
+
+    private final CarPostStoreClient carClient;
+
+    public List<CarPostDto> getCarsForSale(){
+        return carClient.carsForSale();
+    }
+
+    public void changeCarForSale(CarPostDto car, String id){
+        carClient.changeCarForSale(car, id);
+    }
+
+    public void deleteCarForSale(String id){
+        carClient.deleteCarForSale(id);
+    }
+
+}
