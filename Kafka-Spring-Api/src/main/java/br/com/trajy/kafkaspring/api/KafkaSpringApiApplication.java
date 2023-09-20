48011dda8ffacc92344579e4b8ab17256b114b00
Henrique Trajano <henrique.araujo@contmatic.com.br>
2023-09-19 23:25:34 -0300

    kafka-api-initial-commit

---
+++ Kafka-Spring-Api/src/main/java/br/com/trajy/kafkaspring/api/KafkaSpringApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.trajy.kafkaspring.api;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringApiApplication {
+
+	public static void main(String[] args) {
+		run(KafkaSpringApiApplication.class, args);
+	}
+
+}
---
+++ Kafka-Spring-Api/src/test/java/br/com/trajy/kafkaspring/api/KafkaSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trajy.kafkaspring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
