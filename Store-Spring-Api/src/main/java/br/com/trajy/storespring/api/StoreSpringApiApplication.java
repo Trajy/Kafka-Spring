1a80042488c6bc451f5a17b9d6f2758a8bac9398
Trajy <henrique.trajano.a@gmail.com>
2023-09-20 23:55:28 -0300

    added application properties and changed main classes names with prefix Store

--- Store-Spring-Api/src/main/java/br/com/trajy/storespring/api/StoreSpringApiApplication.java
+++ Store-Spring-Api/src/main/java/br/com/trajy/storespring/api/StoreSpringApiApplication.java
@@ -5,10 +5,10 @@ import static org.springframework.boot.SpringApplication.run;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaSpringApiApplication {
+public class StoreSpringApiApplication {
 
 	public static void main(String[] args) {
-		run(KafkaSpringApiApplication.class, args);
+		run(StoreSpringApiApplication.class, args);
 	}
 
 }
